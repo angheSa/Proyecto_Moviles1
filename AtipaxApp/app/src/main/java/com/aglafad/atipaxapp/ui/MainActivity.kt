00c8822a9cbd4421437a13d801a0e14fe2d99de1
hapastorv <alepastor1@hotmail.com>
2023-06-17 16:30:16 -0500

    Login terminado

--- AtipaxApp/app/src/main/java/com/aglafad/atipaxapp/ui/MainActivity.kt
+++ AtipaxApp/app/src/main/java/com/aglafad/atipaxapp/ui/MainActivity.kt
@@ -1,27 +1,24 @@
 package com.aglafad.atipaxapp.ui
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.core.content.ContextCompat
 import androidx.core.widget.addTextChangedListener
-import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import com.aglafad.atipaxapp.databinding.ActivityMainBinding
 import com.aglafad.atipaxapp.entity.Usuario
 import com.aglafad.atipaxapp.room.AlmacenDatabase
 import com.aglafad.atipaxapp.room.UsuarioDao
 import kotlinx.coroutines.flow.Flow
-import com.google.android.material.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import android.graphics.Color
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.ktx.getField
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,13 +28,22 @@ class MainActivity : AppCompatActivity() {
     private lateinit var psw: EditText
     private lateinit var btn_ingresar: Button
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var fbdb: FirebaseFirestore
+    private val rojo = Color.RED
+    private val blanco = Color.WHITE
+    private val negro = Color.BLACK
+    public lateinit var document : DocumentSnapshot
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val intent = Intent(this, SegundaActivity::class.java)
+        //Inicializando FirebaseAuth y Firestore
+        auth = FirebaseAuth.getInstance()
+        fbdb = FirebaseFirestore.getInstance()
+
         user = binding.txtUsuario
         psw = binding.txtPsw
         btn_ingresar = binding.btnIngresar
@@ -50,53 +56,68 @@ class MainActivity : AppCompatActivity() {
             val usuario = user.text.toString()
             val contraseña = psw.text.toString()
             val usuarioDao = db.usuarioDao()
-            var listaUsuarios: Flow<List<Usuario>> = usuarioDao.getUsuarios()
-            val rojo = ContextCompat.getColor(this@MainActivity, R.color.m3_ref_palette_error40)
-            val blanco = ContextCompat.getColor(this@MainActivity, R.color.m3_ref_palette_white)
-
-            val nuevoUsu = Usuario(7, "1234")
-            lifecycleScope.launch {
-                usuarioDao.insert(nuevoUsu)
-            }
-
-            GlobalScope.launch(Dispatchers.Main) {
-                val user = withContext(Dispatchers.IO) {
-                    usuarioDao.getUsuarioLogin(usuario, contraseña)
-                }
-                if (user != null) {
-                    // Usuario válido, realizar acción deseada
-                    Toast.makeText(this@MainActivity, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show()
-                    startActivity(intent)
-                } else {
-                    // Usuario inválido, mostrar mensaje de error
-                    Toast.makeText(this@MainActivity, "Credenciales inválidas", Toast.LENGTH_SHORT).show()
-                    if (usuario == ""){
-                        binding.txtUsuario.setHint("Ingrese usuario")
-                        binding.txtUsuario.setHintTextColor(rojo)
-                        binding.lblUsuario.setTextColor(rojo)
-                    }
-                    if (contraseña == ""){
-                        val rojo = ContextCompat.getColor(this@MainActivity, com.google.android.material.R.color.m3_ref_palette_error40)
-                        binding.txtPsw.setHint("Ingrese contraseña")
-                        binding.txtPsw.setHintTextColor(rojo)
-                        binding.lblPsw.setTextColor(rojo)
-
-                    }
-                }
-                //Recorrido para ver los usuarios en la tabla
-                listaUsuarios.collect {elemento ->
-                    println(elemento)
-                }
-            }
             binding.txtUsuario.addTextChangedListener{
                 binding.txtUsuario.setHint("")
-                binding.lblUsuario.setTextColor(blanco)
+                binding.lblUsuario.setTextColor(negro)
             }
             binding.txtPsw.addTextChangedListener {
                 binding.txtPsw.setHint("")
-                binding.lblPsw.setTextColor(blanco)
+                binding.lblPsw.setTextColor(negro)
             }
-        }
+            checkIfUserExists(usuario, contraseña)
 
+        }
+    }
+    private fun checkIfUserExists(user: String, psw : String) {
+        val intent = Intent(this, SegundaActivity::class.java)
+        if (user == "" && psw == ""){
+            binding.txtUsuario.setHint("Ingrese usuario")
+            binding.txtUsuario.setHintTextColor(rojo)
+            binding.lblUsuario.setTextColor(rojo)
+            binding.txtPsw.setHint("Ingrese contraseña")
+            binding.txtPsw.setHintTextColor(rojo)
+            binding.lblPsw.setTextColor(rojo)
+        }
+        else if (user == ""){
+            binding.txtUsuario.setHint("Ingrese usuario")
+            binding.txtUsuario.setHintTextColor(rojo)
+            binding.lblUsuario.setTextColor(rojo)
+        }
+        else if (psw == ""){
+            binding.txtPsw.setHint("Ingrese contraseña")
+            binding.txtPsw.setHintTextColor(rojo)
+            binding.lblPsw.setTextColor(rojo)
+        }
+        else {
+            fbdb.collection("tb_usuario")
+                .document(user)
+                .get()
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        document = task.result
+                        if (document.exists()) {
+                            // El usuario existe en la colección
+                            // Puedes acceder a los datos del usuario utilizando document.data
+                            if (document.get("password") == psw ) {
+                                startActivity(intent)
+                                Toast.makeText(this@MainActivity,"Bienvenido, " + document.getField("nombre"),Toast.LENGTH_SHORT).show()
+                            }
+                            else
+                                Toast.makeText(
+                                    this@MainActivity,
+                                    "Contraseña no válida",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                        }
+                        else {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "El usuario no existe",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+        }
     }
 }
